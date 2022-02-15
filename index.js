91877e2ca5c164229e4173404c20ed09d8d64dc5
gentakurniawann <muhammad_genta_29rpl@student.smktelkom-mlg.sch.id>
2022-02-15 19:41:19 +0700

    add transaksi

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ app.use(cors());
 
 //import endpoint diletakkan disini
 const admin = require('./routes/admin')
-app.use('/store/admin', admin)
-
-app.listen(8080, () => {
-    console.log("server run on port 8080");
-})
+app.use('/admin', admin)
 
 //endpoint customer
 const customer = require('./routes/customer');
@@ -19,3 +15,11 @@ app.use("/customer", customer)
 //endpoint product
 const product = require('./routes/product');
 app.use("/product", product)
+
+//endpoint transaksi
+const transaksi = require('./routes/transaksi');
+app.use("/transaksi", transaksi)
+
+app.listen(8080, () => {
+    console.log("server run on port 8080");
+})
